9538447360a3560117d455cff3eaddbe5fbaebb2
sasa <ryoki@DESKTOP-2LK94D9.localdomain>
2023-01-11 02:43:21 +0900

    [Massive update]vimscript → Lua

---
+++ .config/nvim/lua/rc/pluginconfig/coc.lua
@@ -0,0 +1,8 @@
+local coc = require("coc")
+coc.setup({
+  options = {
+    coc_global_extentions = ['coc-tsserver', 'coc-eslint', 'coc-prettier', 'coc-git', 'coc-fzf-preview', 'coc-lints'],
+  },
+})
+
+
